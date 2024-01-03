accb9d534853e328ffc5f1b7ae585e0383e09c0e
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-01-03 22:19:35 +0530

    this

---
+++ src/Component/PaymentHistory.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+
+function PaymentHistory({token}){
+
+     const [allPayment , setAllPayment] = useState([]);
+
+
+     const fetchAllPayments = async()=>{
+        
+ try{
+
+    const response = await fetch( "http://localhost:4000/api/v1/payment/fetchAllPayments", {
+        method: "GET",
+        
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+
+      });
+  
+      const formattedResponse = await response.json();
+
+      console.log("for" ,formattedResponse);
+
+      if(formattedResponse.success){
+        setAllPayment(formattedResponse?.allPayments);
+      }
+
+ }catch(error){
+ console.log(error);
+ alert("internal server error , please try again")
+ }    }
+
+
+      useEffect(()=>{
+ fetchAllPayments();
+      },[])
+
+
+    return (
+        <div className="w-full flex flex-col gap-10">
+
+        <h2 className="mx-auto text-white text-[34px] font-[600]">Payment History</h2>
+
+        {
+            allPayment.length > 0 ?(
+              
+ 
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    userName
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    razorpaySignature
+                </th>
+                <th scope="col" class="px-6 py-3">
+                razorpay_payment_id
+
+                </th>
+                <th scope="col" class="px-6 py-3">
+                razorpay_order_id
+
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                allPayment.map((payment)=>(
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {payment?.user.firstName} {payment?.user?.lastName}
+                    </th>
+                    <td class="px-6 py-4">
+                    {payment?.razorpay_order_id}
+
+                    </td>
+                    <td class="px-6 py-4">
+                    {payment?.razorpay_payment_id}
+
+                    </td>
+                    <td class="px-6 py-4">
+                   {payment?.razorpay_signature}
+                    </td>
+                </tr>
+           
+                ))
+            }
+          
+        </tbody>
+    </table>
+</div>
+
+  
+   
+                ):(
+                <span>no payment left </span>
+            )
+        }
+                  
+
+
+              </div>
+    )
+}
+export default PaymentHistory;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import CreateProduct from "../Component/CreateProduct";
 import AllUsers from "../Component/AllUsers";
 import ProductDashboard from "../Component/ProductDashboard";
 import AllCategory from "../Component/AllCategory";
+import PaymentHistory from "../Component/PaymentHistory";
 
 function Dashboard(){
 
@@ -105,6 +106,16 @@ useEffect(()=>{
             </a>
          </li>
          
+         
+         <li onClick={()=>setSelectedItem("paymentHistory")}>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
+               </svg>
+               <span class="flex-1 ms-3 whitespace-nowrap">Payment History</span>
+            </a>
+         </li>
+         
        
         
       </ul>
@@ -138,6 +149,10 @@ useEffect(()=>{
    {
       selectedItem === "category" && 
       <AllCategory updateProductId={updateProductId} setUpdateProductId={setUpdateProductId} updateCategoryId={updateCategoryId} setUpdateCategoryId={setUpdateCategoryId}  token={token} setSelectedItem={setSelectedItem} />
+   }
+   {
+      selectedItem === "paymentHistory" && 
+      <PaymentHistory updateProductId={updateProductId} setUpdateProductId={setUpdateProductId} updateCategoryId={updateCategoryId} setUpdateCategoryId={setUpdateCategoryId}  token={token} setSelectedItem={setSelectedItem} />
    }
 </div>
 
