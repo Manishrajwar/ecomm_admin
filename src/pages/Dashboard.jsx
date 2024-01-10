1b8a954bd3a86d329f0d186019bc0cd686a3380a
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-01-10 20:43:38 +0530

    this

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,8 +7,11 @@ import ProductDashboard from "../Component/ProductDashboard";
 import AllCategory from "../Component/AllCategory";
 import PaymentHistory from "../Component/PaymentHistory";
 import CreateSubCategory from "../Component/CreateSubCategory";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard(){
+   const navigate = useNavigate();
 
    const [openProduct ,setOpenProduct] = useState(false);
    const [openCategory ,setOpenCategory] = useState(false);
@@ -37,6 +40,25 @@ useEffect(()=>{
       setUpdateProductId(sessionStorage.getItem("ecommAdmin_productId"));
     }
 },[setSelectedItem]);
+
+const logoutHandler = ()=>{
+
+   const toastId = toast.loading("Loading...");
+   try{
+      
+      localStorage.removeItem("ecomm_token");
+
+      toast.success("Succesfuly logout");
+
+ navigate("/")
+      
+
+   } catch(error){
+      toast.error("internal server error")
+   }
+
+   toast.dismiss(toastId);
+}
 
     
     return (
@@ -161,6 +183,15 @@ useEffect(()=>{
             }
          </li>
 
+
+         <li onClick={logoutHandler}>
+            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
+               </svg>
+               <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
+            </a>
+         </li>     
 
            
       </ul>
